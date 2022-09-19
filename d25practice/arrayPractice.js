6bc3e2012c272c39da5cba9e5d8d0043a7fabffd
Sharada Khatiwada <saradankhatiwada@gmail.com>
2022-09-18 19:53:38 -0500

    error handle

--- d25practice/arrayPractice.js
+++ d25practice/arrayPractice.js
@@ -11,13 +11,13 @@
 //   includesEvenAge,
 //   includesEvenNum,
 // }; //add all of your function names here that you need for the node mocha tests
-const numArray = [5, 0, 7, 77, -20, 300, 51, 2];
-const peopleArray = [
-  { name: "Sam", age: 15 },
-  { name: "William", age: 6 },
-  { name: "Lucy", age: 13 },
-  { name: "Barney", age: 80 },
-];
+// const numArray = [5, 0, 7, 77, -20, 300, 51, 2];
+// const peopleArray = [
+//   { name: "Sam", age: 15 },
+//   { name: "William", age: 6 },
+//   { name: "Lucy", age: 13 },
+//   { name: "Barney", age: 80 },
+// ];
 function doubleNums(arr) {
   return arr.map((num) => num * 2);
 }
